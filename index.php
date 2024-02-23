ef6cae3c73dcbc9eeafed72d2cc0a77ac08d841a
Valery Pronin <pownmymail@gmail.com>
2024-02-23 23:03:44 +0300

    feat: Testimonial, Services

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header() ?>
 
 <!-- HomeHero -->
-<section class="px-4 mt-56 sm:px-8 lg:px-12">
+<section class="px-4 mt-40 sm:mt-56 lg:mt-80 sm:px-8 lg:px-12">
   <div class="max-w-2xl">
     <h1 class="mt-6 text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl">
       Software designer, founder, and amateur astronaut.
@@ -79,6 +79,90 @@
             <p class="mt-6 text-2xl font-semibold font-display text-zinc-800">Overcome your fears, find your match</p>
             <p class="mt-4 text-base text-zinc-600">Find love in the face of fear — Phobia is a dating app that matches users based on their mutual phobias so they can be scared together.</p>
           </article>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Testimonial -->
+<section class="px-4 py-24 mt-24 sm:py-32 lg:py-40 sm:mt-32 lg:mt-40 sm:px-8 lg:px-12 bg-neutral-50">
+  <figure class="max-w-5xl mx-auto">
+    <blockquote class="relative text-3xl font-medium tracking-tight font-display text-zinc-800 sm:text-4xl">
+      <p class="before:content-['“'] after:content-['”'] sm:before:absolute sm:before:right-full">The team at Studio went above and beyond with our onboarding, even finding a way to access the user’s microphone without triggering one of those annoying permission dialogs.</p>
+    </blockquote>
+    <figcaption class="mt-10">
+      <img alt="ClientName" loading="lazy" width="36" height="36" decoding="async" data-nimg="1" src="<?= get_template_directory_uri() ?>/assets/images/client-logo.svg" class="">
+    </figcaption>
+  </figure>
+
+</section>
+
+<!-- Servises -->
+<section>
+  <div class="px-6 mx-auto mt-24 max-w-7xl lg:px-8 sm:mt-32 lg:mt-40">
+    <div class="max-w-2xl mx-auto lg:max-w-none">
+      <div class="max-w-2xl" style="opacity: 1; transform: none;">
+        <h2>
+          <span class="block mb-6 text-base font-semibold font-display text-zinc-800">Services</span>
+          <span class="sr-only"> - </span>
+          <span class="block font-display tracking-tight [text-wrap:balance] text-4xl font-medium sm:text-5xl text-zinc-800">
+            We help you identify, explore and respond to new opportunities.
+          </span>
+        </h2>
+        <div class="mt-6 text-xl text-neutral-600">
+          <p>
+            As long as those opportunities involve giving us money to re-purpose old projects — we can come up with an endless number of those.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="px-6 mx-auto mt-16 max-w-7xl lg:px-8">
+    <div class="max-w-2xl mx-auto lg:max-w-none">
+      <div class="lg:flex lg:items-center lg:justify-end">
+        <div class="flex justify-center lg:w-1/2 lg:justify-end lg:pr-12">
+          <div class="w-[33.75rem] flex-none lg:w-[45rem]" style="opacity: 1; transform: none;">
+            <div class="justify-center lg:justify-end relative flex aspect-[719/680] w-full grayscale"><svg viewBox="0 0 655 680" fill="none" class="h-full">
+                <g clip-path="url(#:S1:-clip)" class="group">
+                  <g class="transition duration-500 origin-center scale-100 motion-safe:group-hover:scale-105">
+                    <foreignObject width="655" height="680">
+                      <img alt="" loading="lazy" width="2400" height="3000" decoding="async" data-nimg="1" class="object-cover w-full bg-neutral-100 aspect-[655/680] text-transparent" src="<?= get_template_directory_uri() ?>/assets/images/laptop.webp">
+                    </foreignObject>
+                  </g>
+                  <use href="#:S1:-shape" stroke-width="2" class="stroke-zinc-800/10"></use>
+                </g>
+                <defs>
+                  <clipPath id=":S1:-clip">
+                    <path id=":S1:-shape" d="M537.827 9.245A11.5 11.5 0 0 1 549.104 0h63.366c7.257 0 12.7 6.64 11.277 13.755l-25.6 128A11.5 11.5 0 0 1 586.87 151h-28.275a15.999 15.999 0 0 0-15.689 12.862l-59.4 297c-1.98 9.901 5.592 19.138 15.689 19.138h17.275l.127.001c.85.009 1.701.074 2.549.009 11.329-.874 21.411-7.529 24.88-25.981.002-.012.016-.016.023-.007.008.009.022.005.024-.006l24.754-123.771A11.5 11.5 0 0 1 580.104 321h63.366c7.257 0 12.7 6.639 11.277 13.755l-25.6 128A11.5 11.5 0 0 1 617.87 472H559c-22.866 0-28.984 7.98-31.989 25.931-.004.026-.037.035-.052.014-.015-.02-.048-.013-.053.012l-24.759 123.798A11.5 11.5 0 0 1 490.87 631h-29.132a14.953 14.953 0 0 0-14.664 12.021c-4.3 21.502-23.18 36.979-45.107 36.979H83.502c-29.028 0-50.8-26.557-45.107-55.021l102.4-512C145.096 91.477 163.975 76 185.902 76h318.465c10.136 0 21.179-5.35 23.167-15.288l10.293-51.467Zm-512 160A11.5 11.5 0 0 1 37.104 160h63.366c7.257 0 12.7 6.639 11.277 13.755l-25.6 128A11.5 11.5 0 0 1 74.87 311H11.504c-7.257 0-12.7-6.639-11.277-13.755l25.6-128Z" fill-rule="evenodd" clip-rule="evenodd"></path>
+                  </clipPath>
+                </defs>
+              </svg></div>
+          </div>
+        </div>
+        <div>
+          <ul role="list" class="text-base text-zinc-600 mt-16 lg:mt-0 lg:w-1/2 lg:min-w-[33rem] lg:pl-4">
+            <li class="mt-10 group first:mt-0">
+              <div style="opacity: 1; transform: none;">
+                <div class="relative pt-10 group-first:pt-0 group-first:before:hidden group-first:after:hidden before:absolute after:absolute before:bg-zinc-800 after:bg-zinc-800/10 before:left-0 before:top-0 before:h-px before:w-6 after:left-8 after:right-0 after:top-0 after:h-px"><strong class="font-semibold text-zinc-800">Web development. </strong>We specialise in crafting beautiful, high quality marketing pages. The rest of the website will be a shell that uses lorem ipsum everywhere.</div>
+              </div>
+            </li>
+            <li class="mt-10 group first:mt-0">
+              <div style="opacity: 1; transform: none;">
+                <div class="relative pt-10 group-first:pt-0 group-first:before:hidden group-first:after:hidden before:absolute after:absolute before:bg-zinc-800 after:bg-zinc-800/10 before:left-0 before:top-0 before:h-px before:w-6 after:left-8 after:right-0 after:top-0 after:h-px"><strong class="font-semibold text-zinc-800">Application development. </strong>We have a team of skilled developers who are experts in the latest app frameworks, like Angular 1 and Google Web Toolkit.</div>
+              </div>
+            </li>
+            <li class="mt-10 group first:mt-0">
+              <div style="opacity: 1; transform: none;">
+                <div class="relative pt-10 group-first:pt-0 group-first:before:hidden group-first:after:hidden before:absolute after:absolute before:bg-zinc-800 after:bg-zinc-800/10 before:left-0 before:top-0 before:h-px before:w-6 after:left-8 after:right-0 after:top-0 after:h-px"><strong class="font-semibold text-zinc-800">E-commerce. </strong>We are at the forefront of modern e-commerce development. Which mainly means adding your logo to the Shopify store template we’ve used for the past six years.</div>
+              </div>
+            </li>
+            <li class="mt-10 group first:mt-0">
+              <div style="opacity: 1; transform: none;">
+                <div class="relative pt-10 group-first:pt-0 group-first:before:hidden group-first:after:hidden before:absolute after:absolute before:bg-zinc-800 after:bg-zinc-800/10 before:left-0 before:top-0 before:h-px before:w-6 after:left-8 after:right-0 after:top-0 after:h-px"><strong class="font-semibold text-zinc-800">Custom content management. </strong>At Studio we understand the importance of having a robust and customised CMS. That’s why we run all of our client projects out of a single, enormous Joomla instance.</div>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
