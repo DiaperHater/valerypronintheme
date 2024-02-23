22636865c12cb8a522b409187eb98734a9da0da8
Valery Pronin <pownmymail@gmail.com>
2024-02-23 22:39:44 +0300

    feat: HomeHero, Work

--- header.php
+++ header.php
@@ -10,84 +10,89 @@
   <title><?= get_the_title() ?></title>
 </head>
 
-<body class="flex justify-center bg-zinc-50 sm:px-8 lg:px-12">
+<body class="relative flex justify-center bg-zinc-50 sm:px-8 lg:px-12">
   <div class="w-full bg-white max-w-7xl">
 
     <!-- TopNav -->
-    <div class="px-4 sm:px-8 lg:px-12">
-      <header class="relative flex items-start pt-6">
-        <!-- Ava -->
-        <a href="/" class="pointer-events-auto">
-          <img src="<?= get_template_directory_uri() ?>/assets/images/me.jpg" alt="Valery Pronin Ava" class="block object-cover object-center w-10 h-10 rounded-full shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5">
-        </a>
-        <!-- MobileOnly -->
-        <div class="ml-auto md:hidden">
-          <!-- MobileButton -->
-          <button class="sticky flex items-center px-4 py-2 text-sm font-medium rounded-full shadow-lg md:hidden group bg-white/90 text-zinc-800 shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur" onclick="toggleMobileMenu()">
-            Menu
-            <svg viewBox="0 0 8 6" aria-hidden="true" class="w-2 h-auto ml-3 stroke-zinc-500 group-hover:stroke-zinc-700">
-              <path d="M1.75 1.75 4 4.25l2.25-2.5" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-            </svg>
-          </button>
-          <!-- MobileMenu -->
-          <div id="mobile-menu" class="hidden">
-            <div class="fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-sm dark:bg-black/80"></div>
-            <div class="fixed z-50 p-8 origin-top bg-white inset-x-4 top-8 rounded-3xl ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800" id="headlessui-popover-panel-:r1:" tabindex="-1" data-headlessui-state="open">
-              <div class="flex flex-row-reverse items-center justify-between">
-                <button aria-label="Close menu" class="p-1 -m-1" onclick="toggleMobileMenu()">
-                  <svg viewBox="0 0 24 24" aria-hidden="true" class="w-6 h-6 text-zinc-500 dark:text-zinc-400">
-                    <path d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                  </svg>
-                </button>
-                <h2 class="text-sm font-medium text-zinc-600 dark:text-zinc-400">Navigation</h2>
+
+    <div class="fixed top-0 left-0 z-10 flex justify-center w-full sm:px-8 lg:px-12">
+      <div class="w-full max-w-7xl">
+        <div class="px-4 sm:px-8 lg:px-12">
+          <header class="relative flex items-center pt-6">
+            <!-- Ava -->
+            <a href="/" class="pointer-events-auto">
+              <img src="<?= get_template_directory_uri() ?>/assets/images/me.jpg" alt="Valery Pronin Ava" class="block object-cover object-center rounded-full shadow-lg w-14 h-14 ring-1 shadow-zinc-800/5 ring-zinc-900/5">
+            </a>
+            <!-- MobileOnly -->
+            <div class="ml-auto md:hidden">
+              <!-- MobileButton -->
+              <button class="sticky flex items-center px-4 py-2 text-sm font-medium rounded-full shadow-lg md:hidden group bg-white/90 text-zinc-800 shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur" onclick="toggleMobileMenu()">
+                Menu
+                <svg viewBox="0 0 8 6" aria-hidden="true" class="w-2 h-auto ml-3 stroke-zinc-500 group-hover:stroke-zinc-700">
+                  <path d="M1.75 1.75 4 4.25l2.25-2.5" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+              </button>
+              <!-- MobileMenu -->
+              <div id="mobile-menu" class="hidden">
+                <div class="fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-sm dark:bg-black/80"></div>
+                <div class="fixed z-50 p-8 origin-top bg-white inset-x-4 top-8 rounded-3xl ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800" id="headlessui-popover-panel-:r1:" tabindex="-1" data-headlessui-state="open">
+                  <div class="flex flex-row-reverse items-center justify-between">
+                    <button aria-label="Close menu" class="p-1 -m-1" onclick="toggleMobileMenu()">
+                      <svg viewBox="0 0 24 24" aria-hidden="true" class="w-6 h-6 text-zinc-500 dark:text-zinc-400">
+                        <path d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                    </button>
+                    <h2 class="text-sm font-medium text-zinc-600 dark:text-zinc-400">Navigation</h2>
+                  </div>
+                  <!-- Nav -->
+                  <nav class="mt-6">
+                    <ul class="-my-2 text-base divide-y divide-zinc-100 text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+                      <li>
+                        <a class="block py-2" data-headlessui-state="open" href="/about">
+                          About
+                        </a>
+                      </li>
+                      <li>
+                        <a class="block py-2" data-headlessui-state="open" href="/work">
+                          Work
+                        </a>
+                      </li>
+                      <li>
+                        <a class="block py-2" data-headlessui-state="open" href="/contact">
+                          Contact
+                        </a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
               </div>
-              <!-- Nav -->
-              <nav class="mt-6">
-                <ul class="-my-2 text-base divide-y divide-zinc-100 text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                  <li>
-                    <a class="block py-2" data-headlessui-state="open" href="/about">
-                      About
-                    </a>
-                  </li>
-                  <li>
-                    <a class="block py-2" data-headlessui-state="open" href="/work">
-                      Work
-                    </a>
-                  </li>
-                  <li>
-                    <a class="block py-2" data-headlessui-state="open" href="/contact">
-                      Contact
-                    </a>
-                  </li>
-                </ul>
-              </nav>
             </div>
-          </div>
+            <!-- Nav -->
+            <nav class="hidden md:block md:mx-auto">
+              <ul class="flex px-3 text-sm transition rounded-full shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur">
+                <li>
+                  <a href="/about" class="relative block px-3 py-2 transition text-zinc-800 hover:text-teal-500 group active [&.active]:text-teal-500">
+                    About
+                    <span class="absolute h-px opacity-0 inset-x-1 -bottom-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 group-hover:opacity-100 group-[.active]:opacity-100"></span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/work" class="relative block px-3 py-2 transition text-zinc-800 hover:text-teal-500 group">
+                    Work
+                    <span class="absolute h-px opacity-0 inset-x-1 -bottom-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 group-hover:opacity-100 group-[.active]:opacity-100"></span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/contact" class="relative block px-3 py-2 transition text-zinc-800 hover:text-teal-500 group">
+                    Contact
+                    <span class="absolute h-px opacity-0 inset-x-1 -bottom-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 group-hover:opacity-100 group-[.active]:opacity-100"></span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </header>
         </div>
-        <!-- Nav -->
-        <nav class="hidden md:block md:mx-auto">
-          <ul class="flex px-3 text-sm transition rounded-full shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur">
-            <li>
-              <a href="/about" class="relative block px-3 py-2 transition text-zinc-800 hover:text-teal-500 group active [&.active]:text-teal-500">
-                About
-                <span class="absolute h-px opacity-0 inset-x-1 -bottom-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 group-hover:opacity-100 group-[.active]:opacity-100"></span>
-              </a>
-            </li>
-            <li>
-              <a href="/work" class="relative block px-3 py-2 transition text-zinc-800 hover:text-teal-500 group">
-                Work
-                <span class="absolute h-px opacity-0 inset-x-1 -bottom-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 group-hover:opacity-100 group-[.active]:opacity-100"></span>
-              </a>
-            </li>
-            <li>
-              <a href="/contact" class="relative block px-3 py-2 transition text-zinc-800 hover:text-teal-500 group">
-                Contact
-                <span class="absolute h-px opacity-0 inset-x-1 -bottom-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 group-hover:opacity-100 group-[.active]:opacity-100"></span>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      </div>
     </div>
 
     <main>
--- index.php
+++ index.php
@@ -1,15 +1,88 @@
 <?php get_header() ?>
 
-<?php
+<!-- HomeHero -->
+<section class="px-4 mt-56 sm:px-8 lg:px-12">
+  <div class="max-w-2xl">
+    <h1 class="mt-6 text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl">
+      Software designer, founder, and amateur astronaut.
+    </h1>
+    <p class="mt-6 text-xl text-zinc-600">
+      I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.
+    </p>
+  </div>
+</section>
 
-$me = "valery";
-?>
+<!-- Work -->
+<section class="px-4 mt-24 sm:mt-32 lg:mt-40 sm:px-8 lg:px-12">
+  <h2>
+    <span class="block font-display tracking-tight [text-wrap:balance] text-4xl font-medium sm:text-5xl text-zinc-800">Harnessing technology for a brighter future</span>
+  </h2>
+  <div class="mt-6 text-xl text-zinc-600">
+    <p>
+      We believe technology is the answer to the world’s greatest challenges. It’s also the cause, so we find ourselves in bit of a catch 22 situation.
+    </p>
+  </div>
+  <div class="mt-16">
+    <div class="max-w-2xl mx-auto lg:max-w-none">
+      <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
+        <div class="flex" style="opacity: 1; transform: none;">
+          <article class="relative flex flex-col w-full p-6 transition rounded-3xl ring-1 ring-zinc-800/5 hover:bg-zinc-50 sm:p-8">
+            <h3>
+              <a href="/work">
+                <span class="absolute inset-0 rounded-3xl"></span>
+                <img alt="FamilyFund" loading="lazy" width="36" height="36" decoding="async" data-nimg="1" class="w-16 h-16" src="<?= get_template_directory_uri() ?>/assets/images/client-logo.svg">
+              </a>
+            </h3>
+            <p class="flex mt-6 text-sm gap-x-2 text-zinc-800">
+              <time datetime="2023" class="font-semibold">2023</time>
+              <span class="text-zinc-300">/</span>
+              <span>Case study</span>
+            </p>
+            <p class="mt-6 text-2xl font-semibold font-display text-zinc-800">Skip the bank, borrow from those you trust</p>
+            <p class="mt-4 text-base text-zinc-600">FamilyFund is a crowdfunding platform for friends and family. Allowing users to take personal loans from their network without a traditional financial institution.</p>
+          </article>
+        </div>
+        <div class="flex" style="opacity: 1; transform: none;">
+          <article class="relative flex flex-col w-full p-6 transition rounded-3xl ring-1 ring-zinc-800/5 hover:bg-zinc-50 sm:p-8">
+            <h3>
+              <a href="/work">
+                <span class="absolute inset-0 rounded-3xl">
 
-<section class="max-w-4xl px-12 mx-auto mt-24">
-  <h1 class="font-semibold text-red-500 text-5xl text-center">
-    ValeryProninTheme is in da house!
-  </h1>
-  <img src="<?= get_template_directory_uri() ?>/assets/images/me.jpg" alt="Me" class="rounded-lg mt-12 max-w-xl mx-auto">
+                </span>
+                <img alt="Unseal" loading="lazy" width="36" height="36" decoding="async" data-nimg="1" class="w-16 h-16" src="<?= get_template_directory_uri() ?>/assets/images/client-logo.svg">
+              </a>
+            </h3>
+            <p class="flex mt-6 text-sm gap-x-2 text-zinc-800">
+              <time datetime="2022" class="font-semibold">2022</time>
+              <span class="text-zinc-300">/</span>
+              <span>Case study</span>
+            </p>
+            <p class="mt-6 text-2xl font-semibold font-display text-zinc-800">Get a hodl of your health</p>
+            <p class="mt-4 text-base text-zinc-600">Unseal is the first NFT platform where users can mint and trade NFTs of their own personal health records, allowing them to take control of their data.</p>
+          </article>
+        </div>
+        <div class="flex" style="opacity: 1; transform: none;">
+          <article class="relative flex flex-col w-full p-6 transition rounded-3xl ring-1 ring-zinc-800/5 hover:bg-zinc-50 sm:p-8">
+            <h3>
+              <a href="/work">
+                <span class="absolute inset-0 rounded-3xl">
+
+                </span>
+                <img alt="Phobia" loading="lazy" width="36" height="36" decoding="async" data-nimg="1" class="w-16 h-16" src="<?= get_template_directory_uri() ?>/assets/images/client-logo.svg">
+              </a>
+            </h3>
+            <p class="flex mt-6 text-sm gap-x-2 text-zinc-800">
+              <time datetime="2022" class="font-semibold">2022</time>
+              <span class="text-zinc-300" aria-hidden="true">/</span>
+              <span>Case study</span>
+            </p>
+            <p class="mt-6 text-2xl font-semibold font-display text-zinc-800">Overcome your fears, find your match</p>
+            <p class="mt-4 text-base text-zinc-600">Find love in the face of fear — Phobia is a dating app that matches users based on their mutual phobias so they can be scared together.</p>
+          </article>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 
 <?php get_footer() ?>
